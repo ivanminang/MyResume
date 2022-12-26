9f5da8a2d10191dfe1e9b54177daadb7e560de38
ivanminang <ivanminang@gmail.com>
2022-12-26 11:01:32 -0500

    My Resume Cloud

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,8 @@
+/*!
+* Start Bootstrap - Resume v7.0.5 (https://startbootstrap.com/theme/resume)
+* Copyright 2013-2022 Start Bootstrap
+* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-resume/blob/master/LICENSE)
+*/
 //
 // Scripts
 // 
